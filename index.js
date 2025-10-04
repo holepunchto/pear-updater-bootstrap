8d17293eb25fefe702606012743ce34d12707c8c
Mikkel Malmberg <mikkel@brnbw.com>
2025-10-04 14:56:51 +0200

    Apply Prettier

--- index.js
+++ index.js
@@ -6,15 +6,18 @@ const Updater = require('pear-updater')
 const path = require('path')
 const fs = require('fs/promises')
 
-module.exports = async function bootstrap (key, directory = 'pear', {
-  lock = true,
-  bootstrap,
-  onupdater = null,
-  onapply = null,
-  length = 0,
-  fork = 0,
-  force = false
-} = {}) {
+module.exports = async function bootstrap(
+  key,
+  directory = 'pear',
+  {
+    lock = true,
+    bootstrap,
+    onupdater = null,
+    length = 0,
+    fork = 0,
+    force = false
+  } = {}
+) {
   if (!key) throw new Error('key is required')
 
   const corestore = new Corestore(path.join(directory, 'corestores/platform'))
@@ -27,16 +30,18 @@ module.exports = async function bootstrap (key, directory = 'pear', {
     checkout,
     lock: lock ? path.join(directory, 'lock') : null,
     force,
-    swap: force ? await fs.realpath(current) : undefined,
-    onapply
+    swap: force ? await fs.realpath(current) : undefined
   })
 
   const swarm = new Hyperswarm({ bootstrap })
 
   await u.ready()
 
-  const topic = swarm.join(u.drive.discoveryKey, { server: false, client: true })
-  swarm.on('connection', c => corestore.replicate(c))
+  const topic = swarm.join(u.drive.discoveryKey, {
+    server: false,
+    client: true
+  })
+  swarm.on('connection', (c) => corestore.replicate(c))
 
   let serving = false
 
@@ -44,7 +49,10 @@ module.exports = async function bootstrap (key, directory = 'pear', {
   swarm.dht.on('nat-update', function () {
     if (!swarm.dht.randomized && !serving) {
       serving = true
-      swarm.join(u.drive.discoveryKey, { server: true, client: false }).flushed().then(() => topic.destroy())
+      swarm
+        .join(u.drive.discoveryKey, { server: true, client: false })
+        .flushed()
+        .then(() => topic.destroy())
     }
   })
 
